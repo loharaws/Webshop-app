d9521817a7e56b5378eb4ff91fd3be4ab6089d3b
loharaws <127829795+loharaws@users.noreply.github.com>
2026-05-28 15:53:43 +0530

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://opensouArce.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "1.0.0"]
-
+                 [org.slf4j/slf4j-simple "2.0.13"]
                  ;; lifecycle
                  [com.stuartsierra/component "1.0.0"]
 
